acc7fef5715f43debfbd35a66c800b5604e63363
Collins Manase <collomanase@gmail.com>
2022-06-01 02:16:35 +0000

    Print the last digit of the number stored in the variable number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
-#prints a negativenumber from 10000 to -10000
+# prints a negativenumber from 10000 to -10000
 if (number >= 0):
     lastDigit = number % 10
 else:
