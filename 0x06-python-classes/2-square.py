7605129941df46a5ef9d3ff85d8257e7562beb57
Collins Manase <collomanase@gmail.com>
2022-06-22 02:50:27 +0000

    A class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,12 +6,11 @@ class Square:
     """Represents a square."""
 
     def __init__(self, size):
-    """Initializes the data.
-    Private instance attribute: size.
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
-
+        """Initializes the data.
+        Private instance attribute: size.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
