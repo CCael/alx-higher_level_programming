403364c38bb0200ad101cc664f24e7b0b1ebbccb
Collins Manase <collomanase@gmail.com>
2022-06-28 02:16:26 +0000

    Defines a rectangle by: based on 4-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -25,6 +25,9 @@ class Rectangle:
     def __repr__(self):
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
+    def __del__(self):
+        print("Bye rectangle...")
+
     @property
     def width(self):
         return self.__width
