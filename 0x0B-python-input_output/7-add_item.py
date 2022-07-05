ca924518864cdf6c014d2b51682795d7804d73c5
Collins Manase <collomanase@gmail.com>
2022-07-05 23:54:40 +0000

    adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,13 @@
 adds all arguments to a Python list
 """
 
-
+import os
+import json
 import sys
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
