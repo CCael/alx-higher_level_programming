69251a232184086280ea70a1f2b5145f5629ac57
Collins Manase <collomanase@gmail.com>
2022-07-05 23:13:46 +0000

    returns the number of characters added

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 4-append_write
+Appends a string at the end of a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """returns the number of characters added
+    """
+
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
