5d3759ff0bc51fb4193ef145855d0a51b124a3aa
Collins Manase <collomanase@gmail.com>
2022-07-06 01:09:25 +0000

    class Student that defines a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module 9-student
+class Student
+"""
+
+
+class student:
+    """class Student that defines a student
+    public instance attributes:
+            first_name
+            last_name
+            age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        "Public method
+        retrieves a dictionary  representation
+        of a Student instance
+        """
+
+        return obj.__dict__
