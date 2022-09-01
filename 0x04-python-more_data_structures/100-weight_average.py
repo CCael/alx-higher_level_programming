ca42ffaf39e3cfae0dc16be4aa1e37fdb0244865
Collins Manase <collomanase@gmail.com>
2022-08-31 18:33:33 -0700

    Advanced tasks

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num = 0
+    den = 0
+
+    if (my_list and len(my_list) > 0):
+        for (score, weight) in my_list:
+            num += score * weight
+            den += weight
+        num /= den
+
+    return (num)
