9a4253a9cf7843f03ddc9e5723bf4607bfcc53e9
Collins <collomanase@gmail.com>
2022-09-14 19:59:28 -0700

    Add task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -26,8 +26,10 @@ class Square():
 
     """prints in stdout the square with the character #"""
     def my_print(self):
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
         if self.__size == 0:
-            print("")
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
